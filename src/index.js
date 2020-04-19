8a0e3fd322b19dd661e4da053ee5ca811e0fa22d
Tina <longevity.tina@gmail.com>
2020-04-19 11:48:53 -0700

    Adds bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./Home";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <Home />
