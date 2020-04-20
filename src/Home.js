61185b2118ad31a9236042a99e46c70a33a0c08a
Tina <longevity.tina@gmail.com>
2020-04-19 17:06:57 -0700

    Initializes react router detail page

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import SearchContainer from "./SearchContainer";
 import "./Home.scss";
+import { Route, Switch } from "react-router-dom";
+import Details from "./Details";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="Home">
         <h1>Hi there</h1>
-        <SearchContainer />
+        <Switch>
+          <Route path="/details" component={Details} />
+          <Route path="/" component={SearchContainer} />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./Home";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Home />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
